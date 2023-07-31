9f739b65c700f651d487bd4eb79200443c999533
jmulet <mulet.jeremy@gmail.com>
2023-07-31 22:13:57 +0200

    generate detail component

---
+++ src/app/pages/detail/detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss']
+})
+export class DetailComponent implements OnInit {
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    series: [{
+      data: [1, 2, 3],
+      type: 'line'
+    }]
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
