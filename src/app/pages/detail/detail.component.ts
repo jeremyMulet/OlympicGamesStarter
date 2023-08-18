ca5fa94e16aac21e91d76c19583671798cdae45b
jmulet <mulet.jeremy@gmail.com>
2023-08-18 21:54:55 +0200

    Use the local storage to get the last state of the app on page reloaded

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,59 +1,43 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as Highcharts from 'highcharts';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Olympic} from 'src/app/core/models/Olympic';
 import {OlympicService} from 'src/app/core/services/olympic.service';
-import {Subscription} from "rxjs";
 import {ErrorService} from "../../core/services/ErrorService.service";
+import {LocalStorageService} from "../../core/services/local-storage.service";
 
 @Component({
     selector: 'app-detail', templateUrl: './detail.component.html', styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent implements OnInit, OnDestroy {
-    olympics$?: Subscription;
+export class DetailComponent implements OnInit {
+
     pageInfos: { name: string, data: number }[] = [];
     countryData?: Olympic | null | undefined;
     highcharts: typeof Highcharts = Highcharts;
     chartOptions!: Highcharts.Options;
+    isChartDisplayable = false;
+    requestedCountry: string | null = this.route.snapshot.paramMap.get('country');
 
     constructor(private olympicService: OlympicService,
                 private router: Router,
                 private route: ActivatedRoute,
-                private errorService: ErrorService) {}
+                private errorService: ErrorService,
+                private localStorageService: LocalStorageService) {}
 
     ngOnInit(): void {
-        const olympicCountryName = this.route.snapshot.paramMap.get('country');
-
-        this.olympics$ = this.olympicService.getOlympics().subscribe(data => {
-            if (this.olympicService.getOlympicByCountryName(olympicCountryName) !== null) {
-                this.countryData = this.olympicService.getOlympicByCountryName(olympicCountryName);
-            } else {
-                this.errorService.setNotFoundErrorMessage("We cannot found information for the choosen country");
-                this.router.navigateByUrl('**')
-            }
-
-            if (this.countryData !== undefined && this.countryData !== null) {
-                this.pageInfos.push({name: "Number of entries", data: this.countryData?.participations.length})
-                this.pageInfos.push({
-                    name: "Number of medals",
-                    data: this.olympicService.getTotalMedalsForACountry(this.countryData)
-                })
-                this.pageInfos.push({
-                    name: "Number of athletes",
-                    data: this.olympicService.getTotalAthletesForACountry(this.countryData)
-                })
-                this.initLineChart();
-            }
-        });
-    }
-
-    ngOnDestroy(): void {
-        if (this.olympics$) {
-            this.olympics$.unsubscribe();
+        this.countryData = this.olympicService.getOlympicByCountryName(this.requestedCountry);
+        if (this.countryData !== undefined) {
+            this.localStorageService.setItem("currentCountryData", this.countryData);
+            this.initHeader();
+            this.initLineChart();
+        } else {
+            this.checkForExistingStorageData();
         }
     }
 
     initLineChart(): void {
+        this.isChartDisplayable = true;
+        console.log("init line chart")
         const datas: number[] = [];
         const categories: string[] = [];
 
@@ -66,10 +50,7 @@ export class DetailComponent implements OnInit, OnDestroy {
             colors: ['#956065'],
             title: {text: ''},
             xAxis: {
-                title: {
-                    text: 'Dates',
-                    style: { fontSize: '25px' }
-                },
+                title: {text: 'Dates', style: { fontSize: '25px' }},
                 categories: categories,
             },
             yAxis: {
@@ -77,9 +58,7 @@ export class DetailComponent implements OnInit, OnDestroy {
             },
             tooltip: {
                 backgroundColor: 'rgb(2, 131, 143)', borderWidth: 0, borderRadius: 10, shadow: false,
-                style: {
-                    color: '#F0F0F0', textAlign: 'center'
-                },
+                style: {color: '#F0F0F0', textAlign: 'center'},
                 useHTML: true, formatter: function () {
                     return '<i class="fas fa-medal"></i><b> ' + this.y + '</b>';
                 }
@@ -87,6 +66,31 @@ export class DetailComponent implements OnInit, OnDestroy {
             legend: {enabled: false},
             series: [{type: 'line', data: datas}],
         };
+    }
+
+    initHeader(): void {
+        this.pageInfos.length = 0;
+        this.pageInfos.push({name: "Number of entries", data: this.countryData!.participations.length});
+        this.pageInfos.push({
+            name: "Number of medals",
+            data: this.olympicService.getTotalMedalsForACountry(this.countryData!)
+        });
+        this.pageInfos.push({
+            name: "Number of athletes",
+            data: this.olympicService.getTotalAthletesForACountry(this.countryData!)
+        });
+    }
+
+    checkForExistingStorageData(): void {
+        this.countryData = this.localStorageService.getItem("currentCountryData");
+        if (this.countryData != null && this.countryData.country == this.requestedCountry) {
+            this.initHeader();
+            this.initLineChart();
+        } else  {
+            this.errorService.setNotFoundErrorMessage("We cannot found information for the chosen country");
+            this.router.navigateByUrl('**');
+        }
+
     }
 
 }
